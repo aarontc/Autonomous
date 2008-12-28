dc0362de52f8c9ce9135816c54860915cef9b3c0
bjmn55 <bjmn55@627d9db2-2651-0410-8e54-ae66a1c21b7d>
2008-12-28 01:26:44 +0000

    git-svn-id: file:///home/aaron/Optional/autonomous/trunk@37 627d9db2-2651-0410-8e54-ae66a1c21b7d

--- webinterface/rules2.php
+++ webinterface/rules2.php
@@ -1,8 +1,9 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 	<head>
 		<title>Autonomous</title>
 		<meta content="">
-		<link href='style2.css' type='text/css' media='screen,projection' rel='stylesheet' />
+		<link href='style2.css?<?= md5 ( time () ); ?>' type='text/css' media='screen,projection' rel='stylesheet' />
 	</head>
 	<body>
 	<div id='header' class='area'>
@@ -12,10 +13,10 @@
 		</div>
 		<div id='hright'>
 			<div class='bgcolor headerlg'>&nbsp;</div>
-			<div class='dkgrey'>Search for Term:<input type='text' /><input type='submit' value='GO' /></div>
+			<div class='dkgrey'>Search for Term:<input type='text' class='text' /><input type='submit' class='button' value='GO' /></div>
 		</div>
 	</div>
-	<div id='spacer'></div>
+	<div id='divider'><!-- comment for IE --></div>
 	<div id='main' class='area'>
 		<div id='sidebar'>
 			<div id='menuitem'>
@@ -24,21 +25,40 @@
 			</div>
 		</div>
 		<div id='content'>
-			<div id='rules'>
+			<div id='rules' class='area'>
 				<!-- rule starts here -->
-				<div id='rule'> 
-					<div id='ruletitle' class='area'>
-						<input type='text' />
-						<input type='image' title='Delete' alt='Delete' value='Delete' src='delete-15x15.png' />
+				<div id='rule'>
+					<div id='ruletitle'>
+						<input type='text' class='text' />
+						<input type='image' class='image' title='Delete' alt='Delete' value='Delete' src='delete-15x15.png' />
 					</div>
 					<div id='rulebody'>
 						<div class='area'>
-							<p><label>LAN Computer IP</label><input type='text' style='width:120px;' /></p>
+							<p><label>LAN Computer IP</label><input type='text' class='text' style='width:120px;' /></p>
 							<p><label>Protocal</label><select><option value='tcp'>TCP</option><option value='udp'>UDP</option></select></p>
 						</div>
 						<div class='area'>
-							<p><label>LAN Port</label><input type='text' style='width:40px;' /><input type='text' style='width:40px;' /></p>
-							<p><label>Internet Port</label><input type='text' style='width:40px;' /><input type='text' style='width:40px;' /></p>
+							<p><label>LAN Port</label><input type='text' class='text' style='width:40px;' /><input type='text' class='text' style='width:40px;' /></p>
+							<p><label>Internet Port</label><input type='text' class='text' style='width:40px;' /><input type='text' class='text' style='width:40px;' /></p>
+						</div>
+					</div>
+				</div>
+				<!-- rule ends here -->
+				<div id='rulespacer'></div>
+				<!-- rule starts here -->
+				<div id='rule' style='float: left;'>
+					<div id='ruletitle'>
+						<input type='text' class='text' />
+						<input type='image' class='image' title='Delete' alt='Delete' value='Delete' src='delete-15x15.png' />
+					</div>
+					<div id='rulebody'>
+						<div class='area'>
+							<p><label>LAN Computer IP</label><input type='text' class='text' style='width:120px;' /></p>
+							<p><label>Protocal</label><select><option value='tcp'>TCP</option><option value='udp'>UDP</option></select></p>
+						</div>
+						<div class='area'>
+							<p><label>LAN Port</label><input type='text' class='text' style='width:40px;' /><input type='text' class='text' style='width:40px;' /></p>
+							<p><label>Internet Port</label><input type='text' class='text' style='width:40px;' /><input type='text' class='text' style='width:40px;' /></p>
 						</div>
 					</div>
 				</div>
@@ -46,9 +66,9 @@
 			</div>
 		</div>
 	</div>
-	<div id='spacer'></div>
+	<div id='divider'><!-- comment for IE --></div>
 	<div id='footer' class='dkgrey'>
-		Copyright &copy; 2008, <a href='http://www.principleofdesign.com' class='dkgrey'>Scott Deutsch, Ben Mann, Aaron Ten Clay</a>. All Rights Reserved.
+		Copyright &copy; 2008, Scott Deutsch, Ben Mann, Aaron Ten Clay. All Rights Reserved.
 	</div>
 	</body>
 </html>
